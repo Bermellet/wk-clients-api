427a79a01e0b65d854f8f67022f7d8cf0fe143ae
Bermellet <jnietomaldonado@gmail.com>
2026-03-10 00:11:19 +0100

    Swagger configuration

--- WKClientsApi/WKClientsApi/Program.cs
+++ WKClientsApi/WKClientsApi/Program.cs
@@ -1,19 +1,23 @@
 using WKClientsApi.Endpoints;
 using WKClientsApi.Repositories;
+using Microsoft.OpenApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Services
 builder.Services.AddSingleton<IClienteRepository, JsonClienteRepository>();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "WK Clients API", Version = "v1" });
+});
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WK Clients API V1"));
 }
 
 // Mapeo de Endpoints
--- WKClientsApi/WKClientsApi/Repositories/JsonClienteRepository.cs
+++ WKClientsApi/WKClientsApi/Repositories/JsonClienteRepository.cs
@@ -1,24 +1,27 @@
-﻿using System.Text.Json;
+﻿using System.IO;
+using System.Text.Json;
 using WKClientsApi.Models;
 
 namespace WKClientsApi.Repositories
 {
     public class JsonClienteRepository : IClienteRepository
     {
-        private readonly string _filePath = "data/clientes.json";
+        private readonly string _filePath = "data/clientes_store.json";
         private readonly JsonSerializerOptions _options = new() { PropertyNameCaseInsensitive = true, WriteIndented = true };
 
         private async Task<List<Cliente>> LoadData()
         {
-            if (!File.Exists(_filePath)) return new List<Cliente>();
-            var json = await File.ReadAllTextAsync(_filePath);
+            var filePath = GetCompleteFilePath(_filePath);
+            if (!File.Exists(filePath)) return new List<Cliente>();
+            var json = await File.ReadAllTextAsync(filePath);
             return JsonSerializer.Deserialize<List<Cliente>>(json, _options) ?? new List<Cliente>();
         }
 
         private async Task SaveData(List<Cliente> clientes)
         {
             var json = JsonSerializer.Serialize(clientes, _options);
-            await File.WriteAllTextAsync(_filePath, json);
+            var filePath = GetCompleteFilePath(_filePath);
+            await File.WriteAllTextAsync(filePath, json);
         }
 
         public async Task<IEnumerable<Cliente>> GetAllAsync() => await LoadData();
@@ -38,6 +41,12 @@ namespace WKClientsApi.Repositories
             var clientes = await LoadData();
             clientes.RemoveAll(c => c.DNI == dni);
             await SaveData(clientes);
+        }
+
+        private string GetCompleteFilePath(string filePath)
+        {
+            var baseDir = AppDomain.CurrentDomain.BaseDirectory;
+            return Path.Combine(baseDir, filePath);
         }
     }
 }
